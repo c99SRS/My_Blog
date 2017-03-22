f151214f2c64ed2b02964a2dea801ba1fe2ab0d5
c99SRS <smruti.work@gmail.com>
2017-03-22 22:48:38 +0530

    changr hostname

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2lx7@$34f@uc*_f$8n*@*orj^-qr+1zjg4go@*x7&_4w3%58c1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
